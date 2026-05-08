804920705a945cd282ef9d286550f3fbcbddcbb0
kashishfatima999 <kashishfatima999@gmail.com>
2026-05-08 07:10:03 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,17 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// RESTORED BACKEND LOGGING
 app.use(morgan('dev'));
+app.use((req, res, next) => {
+  const start = Date.now();
+  res.on('finish', () => {
+    const duration = Date.now() - start;
+    console.log(`${req.method} ${req.originalUrl} ${res.statusCode} - ${duration}ms`);
+  });
+  next();
+});
 
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Backend working' });
